3e4b7894f756cdf5f5df4fb268a174db0b6915f2
Harutyun-Yan <harutyun.har@gmail.com>
2022-06-03 17:03:19 +0400

    Add project files.

---
+++ Extention.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable
+{
+    public static class Extention
+    {
+        public static IEnumerable<int> _First(this IEnumerable<int> source)
+        {
+            if (source == null)
+                throw new ArgumentException();
+            foreach (var item in source)
+            {
+                yield return item;
+                yield break;
+            }
+        }
+        public static IEnumerable<int> PlusChange(this IEnumerable<int> source, int value)
+        {
+            if (source == null)
+                throw new ArgumentException();
+            foreach (var item in source)
+            {
+                yield return (item + value);
+                yield break;
+            }
+        }
+        public static IEnumerable<int> MinusChange(this IEnumerable<int> source, int value)
+        {
+            if (source == null)
+                throw new ArgumentException();
+            foreach (var item in source)
+            {
+                yield return (item - value);
+                yield break;
+            }
+        }
+
+        public static IEnumerable<int> MultyChange(this IEnumerable<int> source, int value)
+        {
+            if (source == null)
+                throw new ArgumentException();
+            foreach (var item in source)
+            {
+                yield return (item * value);
+                yield break;
+            }
+        }
+        public static IEnumerable<int> DivChange(this IEnumerable<int> source, int value)
+        {
+            if (source == null && value == 0)
+                throw new ArgumentException();
+            foreach (var item in source)
+            {
+                yield return (item / value);
+                yield break;
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> res = new List<int> { 5, 28, 12, 6, -6, -36, 79 };
+            List<int> ress = new List<int>();
+
+            var test1 = res._First();
+            var test2 = ress._First();
+            var test3 = res.PlusChange(10);
+            var test4 = ress.PlusChange(10);
+            var test5 = res.MinusChange(10);
+            var test6 = res.MultyChange(10);
+            var test7 = res.DivChange(0);
+        }
+    }
+}
